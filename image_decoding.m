64b9176f14cb4b656ce7de3dd4662fc31d2c30a6
tingleshao <cshao@cs.unc.edu>
2017-02-25 22:21:59 -0500

    load iDCT, implement reading from CSV

--- image_decoding.m
+++ image_decoding.m
@@ -1,11 +1,15 @@
 % read input image
-
+img = imread('android-icon.jpg');
 
 % downsample 
-
+img = imresize(img, [64,64]);
 
 % convert to dct
+imgdct = dct2(img(:,:,1));
+imgdctinv = idct2(imgdct);
+
 
 % output bit stream
+csvwrite('android-icon.csv',imgdct)
 
 
